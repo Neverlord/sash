5c38a977d3f40a417d9d4e23e3dbd5e14f5a330c
Dominik Charousset <dominik.charousset@haw-hamburg.de>
2014-07-13 00:10:58 +0200

    Implemented variables engine
    The new engine is simple but powerful enough for most use cases.

--- sash/variables_engine.hpp
+++ sash/variables_engine.hpp
@@ -21,6 +21,7 @@
 
 #include <map>
 #include <string>
+#include <iterator>
 #include <algorithm>
 
 namespace sash {
@@ -40,35 +41,150 @@ public:
   void parse(std::string& err, const std::string& in,
              std::string& out, bool sub_parse = false)
   {
-    auto valid_char = [](char c)
+    auto valid_varname_char = [](char c)
     {
       return std::isalnum(c) || c == '_';
     };
     using iter = typename std::string::const_iterator;
+    char c = '\0'; // current character
     char lastc = ' '; // the last character, needed to distinct "$" from "\$"
     iter pos = in.begin(); // our current position in the stream
     iter last = in.end();
-    for (auto i = in.begin(); i != last; ++i)
+    enum
     {
-      switch (*i)
+        // traversing input, copying characters as we go
+        traverse,
+        // just read a $ indicating the start of a variable name
+        after_dollar_sign,
+        // read a variable name in the form of $name
+        read_variable,
+        // read a variable name in the form of ${name}
+        read_braced_variable
+    }
+    state = traverse;
+    auto i = in.begin();
+    raii_error_string scoped_err{err};
+    auto set_error = [&]() -> std::ostream&
+    {
+      return scoped_err.oss << "syntax error at position "
+                            << std::to_string(std::distance(in.begin(), i))
+                            << ": ";
+    };
+    auto flush = [&]() -> bool
+    {
+        switch (state)
+        {
+          case traverse:
+            if (pos != last)
+              out.insert(out.end(), pos, i);
+            return true;
+          case after_dollar_sign:
+            if (i == last)
+            {
+              set_error() << "$ at end of line";
+            }
+            else if (*i == '$')
+            {
+              set_error() << "$$ is not a valid expression";
+            }
+            else
+            {
+              set_error() << "unexpected character '"
+                          << *i << "' after $";
+            }
+            out.clear();
+            return false;
+          // pos is always set to the first character of the variable name,
+          // i is always set to the first character that's not part of it
+          case read_braced_variable:
+            if (! valid_varname_char(c) && c != '}')
+            {
+              set_error() << "'" << c
+                          << "' is an invalid character inside ${...}";
+              return false;
+            }
+            // else:: fall through
+          case read_variable:
+            std::string varname(pos, i);
+            auto j = variables_.find(varname);
+            out += j == variables_.end() ? "" : j->second;
+            state = traverse;
+            if (state == read_braced_variable)
+              pos = i + 1;
+            else
+              pos = i;
+            return true;
+        }
+        return false; // should be unreachable
+    };
+    while (i != last)
+    {
+      c = *i;
+      switch (c)
       {
         case '=':
           // assignment lines are parsed as '([a-zA-Z0-9_]+)=(.+)' => check
-          if (! sub_parse && i != pos && pos == in.begin() && std::all_of(pos, i, valid_char))
+          if (! sub_parse && i != pos && pos == in.begin()
+              && std::all_of(pos, i, valid_varname_char))
           {
               std::string key(pos, i);
               std::string val_input(i + 1, in.end());
               std::string value;
+              // assignments don't produce an output
+              out.clear();
               // our value can in turn have variables
               parse(err, val_input, value, true);
-              variables_.emplace(std::move(key), std::move(value));
-              out.clear();
+              if (err.empty())
+                variables_.emplace(std::move(key), std::move(value));
               return;
           }
+          break;
+        case '$':
+          if (lastc == '\\')
+            break;
+          // write previous input
+          if (! flush())
+            return;
+          state = after_dollar_sign;
+          pos = i + 1;
+          break;
+        case '{':
+          if (state == after_dollar_sign)
+          {
+            pos = i + 1; // '{' is not part of the variable name
+            state = read_braced_variable;
+          }
+          break;
+        case '}':
+          if (state == read_braced_variable)
+          {
+            if (! flush())
+              return;
+            pos = i + 1; // '}' won't be part of the output
+            break;
+          }
+          // else: fall through
+        default:
+          if (! valid_varname_char(c) && state != traverse)
+          {
+            if (! flush())
+              return;
+          }
+          else if (state == after_dollar_sign)
+          {
+            pos = i;
+            state = read_variable;
+          }
+          break;
       }
-      lastc = *i;
+      lastc = c;
+      ++i;
     }
-    if (pos != last) out.insert(out.end(), pos, last);
+    if (state == read_braced_variable)
+    {
+      err = "syntax error: missing '}' at end of line";
+    }
+    flush();
   }
 
   /// Factory function to create a std::function using this implementation.
@@ -84,6 +200,23 @@ public:
   }
 
 private:
+  struct raii_error_string
+  {
+    raii_error_string(std::string& ref) : err{ref}
+    {
+      // nop
+    }
+    ~raii_error_string()
+    {
+      // leave err untouched if no error occured
+      auto str = oss.str();
+      if (! str.empty())
+        err = std::move(str);
+    }
+    std::ostringstream oss;
+    std::string& err;
+  };
+
   Container variables_;
 };
 
